bea6c66cb530a77f4b07861c63132478eba98113
Swan Htet Aung <swanhtetaung308@gmail.com>
2025-12-14 16:40:32 +0700

    Learning assets

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
     <Home />
+    <img src="@/assets/chair.jpg" alt="Chair Image" />
+    <IconBurger class="w-8 h-8" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  alias: {
+    assets: "./app/assets",
+  },
   css: ["./app/assets/main.css"],
   vite: {
     plugins: [tailwindcss()],
